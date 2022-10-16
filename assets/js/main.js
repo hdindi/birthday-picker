bfd37ff53213e450ae077ad0979208a911f1a6d2
Dindi Harris Samuel <harrisdindisamuel@gmail.com>
2022-10-16 14:39:45 +0300

    gender selector function

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,15 +11,36 @@ $(document).ready(function () {
     $("#calculate_age").click(function (e) {
         e.preventDefault();
         birth_date = $("#BithDate").val();
+        gender = $("#Gender").val();
 
-        var d = new Date(birth_date);
-        var n = d.getDay(d);
+        var convertDate = new Date(birth_date);
+        var dayWeek = convertDate.getDay();
 
-        console.log(n)
+        console.log(dayWeek)
 
-       
+        switch (gender) {
+            case "female":
+                femaleSelector(dayWeek);
+                break;
+            case "male":
+                maleSelector(dayWeek);
+                break;
+            case "":
+                break;
+        }
+
+
 
     });
+
+    function maleSelector(dayWeek) {
+        console.log("Male selector ")
+    }
+
+
+    function femaleSelector(dayWeek) {
+        console.log("Female selector ")
+    }
 
 
 
