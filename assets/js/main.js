88337ea764e58b4c11f19b6f8c9c873da4ac0426
Dindi Harris Samuel <harrisdindisamuel@gmail.com>
2022-10-16 14:23:29 +0300

    removed the weekday calculator function -> didn't meet the  expexpextation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
+
+/**
+ * TO DO = > Add functionality to calculate Day of the  week
+ * Add functionality to calculate Day of the  week
+ */
+// TO DO  Add functionality to calculate Day of the  week 
+
 $(document).ready(function () {
 
 
@@ -5,32 +12,15 @@ $(document).ready(function () {
         e.preventDefault();
         birth_date = $("#BithDate").val();
 
-      
+        var d = new Date(birth_date);
+        var n = d.getDay(d);
 
-        weekDayCalculator(birth_date);
+        console.log(n)
+
+       
 
     });
 
 
-    function weekDayCalculator(birth_date) {
-        /**
-         * Date format -> YYYY-MM-DD
-         * CC - > Century
-         * YY - > 2 Digits of the Yr
-         * MM - > Month 
-         * DD -> Day of the Month         
-         */
-        var CC = birth_date.substr(0, 2);
 
-        var YY = birth_date.substr(2, 2);
-        var MM = birth_date.substr(5, 2);
-        var DD = birth_date.substr(8, 2);
-
-        console.log(`CC => ${CC} YY => ${YY} MM => ${MM} DD=> ${DD} `)
-
-        var dayOfWeek = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7;
-        console.log(dayOfWeek)
-
-        return dayOfWeek;
-    }
 });
